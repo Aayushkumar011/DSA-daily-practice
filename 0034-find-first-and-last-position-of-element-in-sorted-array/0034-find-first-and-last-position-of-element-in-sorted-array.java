38fe12325695e5163438e7c0a46d8d7a1ac6f5e5
Aayush Kumar <roraayush0001@gmail.com>
2025-09-28 20:43:46 +0530

    Time: 0 ms (100%), Space: 46 MB (69.94%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,36 +1,42 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        int[] arr = {-1,-1};
-        int start = 0;
-        int end = nums.length-1;
-        while(start<=end){
-            int mid = start+(end-start)/2;
+    public static void first(int[] nums, int target, int[] arr){
+        int f = 0;
+        int l = nums.length-1;
+        while(f<=l){
+            int mid = f+(l-f)/2;
             if(nums[mid]==target){
                 arr[0]=mid;
-                end=mid-1;
+                l=mid-1;
             }
-            else if(nums[mid]<target){
-                start=mid+1;
+            else if(nums[mid]>target){
+                l=mid-1;
             }
             else{
-                end=mid-1;
+                f=mid+1;
             }
         }
-        start = 0;
-        end = nums.length-1;
-        while(start<=end){
-            int mid = start+(end-start)/2;
+    }
+    public static void last(int[] nums, int target, int[] arr){
+        int f = 0;
+        int l = nums.length-1;
+        while(f<=l){
+            int mid = f+(l-f)/2;
             if(nums[mid]==target){
                 arr[1]=mid;
-                start=mid+1;
+                f=mid+1;
             }
-            else if(nums[mid]<target){
-                start=mid+1;
+            else if(nums[mid]>target){
+                l=mid-1;
             }
             else{
-                end=mid-1;
+                f=mid+1;
             }
         }
+    }
+    public int[] searchRange(int[] nums, int target) {
+        int[] arr = {-1,-1};
+        first(nums,target,arr);
+        last(nums,target,arr);
         return arr;
     }
 }
