2c033687795ea203de76a2d3d3873ebafa60fb72
Aayush Kumar <roraayush0001@gmail.com>
2025-09-30 22:15:44 +0530

    Time: 5 ms (95.49%), Space: 43 MB (49.77%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,18 +1,26 @@
 class Solution {
-    public static boolean helper(String s1,String s2){
-        char[] a1 = s1.toCharArray();
-        char[] a2 = s2.toCharArray();
-        Arrays.sort(a1);
-        Arrays.sort(a2);
-        return Arrays.equals(a1, a2);
-    }
     public boolean checkInclusion(String s1, String s2) {
-        int w = s1.length();
-        int l = s2.length();
-        int n = l-w+1;
+        int n =  s1.length();
+        int m = s2.length();
+        if(n>m)return false;
+        int window = n;
+        int[] f1 = new int[26];
+        int[] f2 = new int[26];
+        // frequency of s1 string;
         for(int i=0;i<n;i++){
-            if(helper(s1,s2.substring(i,w+i)))return true;
+            f1[s1.charAt(i)-'a']++;
         }
-    return false;
+        // frequency of first window;
+        for(int i=0;i<window;i++){
+            f2[s2.charAt(i)-'a']++;
+        } 
+        if(Arrays.equals(f1,f2))return true;
+        // check for others windows;
+        for(int i=window;i<m;i++){
+            f2[s2.charAt(i-window)-'a']--;
+            f2[s2.charAt(i)-'a']++;
+            if(Arrays.equals(f1,f2))return true;
+        }
+        return false;
     }
 }
