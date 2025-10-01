e57a38c45128faa4290f886215642ce9131c0625
Aayush Kumar <roraayush0001@gmail.com>
2025-10-01 15:38:29 +0530

    Time: 6 ms (85.02%), Space: 42.2 MB (99.35%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,21 +1,19 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int n =  s1.length();
-        int m = s2.length();
+      
+        int n=s1.length();
+        int m=s2.length();
         if(n>m)return false;
-        int window = n;
+        int window=n;
         int[] f1 = new int[26];
         int[] f2 = new int[26];
-        // frequency of s1 string;
         for(int i=0;i<n;i++){
             f1[s1.charAt(i)-'a']++;
         }
-        // frequency of first window;
         for(int i=0;i<window;i++){
             f2[s2.charAt(i)-'a']++;
         } 
         if(Arrays.equals(f1,f2))return true;
-        // check for others windows;
         for(int i=window;i<m;i++){
             f2[s2.charAt(i-window)-'a']--;
             f2[s2.charAt(i)-'a']++;
