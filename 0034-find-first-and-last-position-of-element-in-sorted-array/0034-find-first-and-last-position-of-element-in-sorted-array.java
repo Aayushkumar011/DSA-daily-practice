232d7e2472ac3ddbec2c9b33cb96327beffe43e0
Aayush Kumar <roraayush0001@gmail.com>
2025-10-17 16:05:36 +0530

    Time: 0 ms (100%), Space: 46.3 MB (17.61%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,42 +1,34 @@
 class Solution {
-    public static void first(int[] nums, int target, int[] arr){
-        int f = 0;
-        int l = nums.length-1;
-        while(f<=l){
-            int mid = f+(l-f)/2;
-            if(nums[mid]==target){
-                arr[0]=mid;
-                l=mid-1;
-            }
-            else if(nums[mid]>target){
-                l=mid-1;
-            }
-            else{
-                f=mid+1;
-            }
-        }
-    }
-    public static void last(int[] nums, int target, int[] arr){
-        int f = 0;
-        int l = nums.length-1;
-        while(f<=l){
-            int mid = f+(l-f)/2;
-            if(nums[mid]==target){
-                arr[1]=mid;
-                f=mid+1;
-            }
-            else if(nums[mid]>target){
-                l=mid-1;
-            }
-            else{
-                f=mid+1;
-            }
-        }
-    }
     public int[] searchRange(int[] nums, int target) {
-        int[] arr = {-1,-1};
-        first(nums,target,arr);
-        last(nums,target,arr);
-        return arr;
+        int[] ans = new int[2];
+        ans[0] = -1;
+        ans[1] = -1;
+        int i=0;
+        int j=nums.length-1;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            if(nums[mid]==target){
+                ans[0] = mid;
+                j=mid-1;
+            }
+            else if(nums[mid]<target){
+                i=mid+1;
+            }
+            else j=mid-1;
+        }
+        i = 0;
+        j = nums.length-1;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            if(nums[mid]==target){
+                ans[1] = mid;
+                i=mid+1;
+            }
+            else if(nums[mid]<target){
+                i=mid+1;
+            }
+            else j=mid-1;
+        }
+    return ans;
     }
 }
