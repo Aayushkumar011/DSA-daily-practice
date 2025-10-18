e8828f6773bad20e42f7d82a9e7f633e6216c2e2
Aayush Kumar <roraayush0001@gmail.com>
2025-10-18 19:50:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -1,17 +1,36 @@
 class Solution {
+    public static int Miss(int[] arr, int k, int mid){
+        int lower = -1;
+        int i=0;int j=arr.length-1;
+        int ans = 0;
+        while(i<=j){
+            int m = i+(j-i)/2;
+            if(arr[m]<=mid){
+                ans=m;
+                i=m+1;
+            }
+            else j=m-1;
+        }
+    return ans+1;
+    }
     public int findKthPositive(int[] arr, int k) {
-        int max = Integer.MIN_VALUE;
-        for(int i=0;i<arr.length;i++)max=Math.max(max,arr[i]);
-        int l = max+k;
-        int[] f = new int[l+1];
-        for(int i=0;i<arr.length;i++){
-            f[arr[i]]++;
+        int i=1;
+        int max=Integer.MAX_VALUE;
+        for(int l=0;l<arr.length;l++){
+            max=Math.max(max,arr[l]);
         }
-        int j=1;
-        while(k>0){
-            if(f[j]==0)k--;
-            j++;
+        int j=max;
+        while(i<=j){
+            int mid = i+(j-i)/2;
+            int miss = Miss(arr, k, mid);
+            if(mid-miss==k){
+                return mid;
+            }
+            else if(mid-miss>k){
+                j=mid-1;
+            }
+            else i=mid+1;
         }
-    return j-1;
+    return -1;
     }
 }
