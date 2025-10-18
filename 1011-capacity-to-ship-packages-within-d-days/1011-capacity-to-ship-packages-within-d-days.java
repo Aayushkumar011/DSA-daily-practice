ccbbc9fe430f58c96c02acba17c02f4b467e8d8f
Aayush Kumar <roraayush0001@gmail.com>
2025-10-18 17:42:50 +0530

    Time: 16 ms (6.21%), Space: 46.7 MB (98.88%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,35 +1,39 @@
 class Solution {
-    public static boolean delivered(int[] weights, int days, int m){
-        int c=0;
-        int sum=0;
-        for(int e:weights){
-            sum+=e;
-            if(sum>m){
+    public static boolean canShip(int[] weights, int days, long mid){
+        int c = 0;
+        long sum = 0;
+        for(int i=0;i<weights.length;i++){
+            sum+=weights[i];
+            if(sum==mid){
                 c++;
-                sum=e;
+                sum=0;
+            }
+            else if(sum>mid){
+                c++;
+                sum=weights[i];
             }
         }
-        c++;
-        return c<=days;
+        if(sum!=0)c++;
+    return c<=days;
     }
     public int shipWithinDays(int[] weights, int days) {
-        int l=0;
-        int r=0;
-        int ans=0;
-        for(int e:weights) {
-            l = Math.max(l,e); 
-            r += e;           
+        long sum=0;
+        long max = Integer.MIN_VALUE;
+        for(int k=0;k<weights.length;k++){
+            sum+=weights[k];
+            max=Math.max(max,weights[k]);
         }
-        while(l<=r){
-            int mid = l+(r-l)/2;
-            if(delivered(weights,days,mid)){
-                ans=mid;
-                r=mid-1;
+        long i=max;
+        long j=sum;
+        int ans = 0;
+        while(i<=j){
+            long mid = i+(j-i)/2;
+            if(canShip(weights,days,mid)){
+                ans = (int)mid;
+                j=mid-1;
             }
-            else{
-                l=mid+1;
-            }
+            else i=mid+1;
         }
-        return ans;
+    return ans;
     }
 }
