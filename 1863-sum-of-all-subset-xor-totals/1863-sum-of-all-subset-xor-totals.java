a2de9adc667c08d5e07c834c3c81ef2cd12f8e67
Aayush Kumar <roraayush0001@gmail.com>
2025-12-29 22:43:37 +0530

    Time: 5 ms (24.92%), Space: 42.9 MB (25.44%) - LeetHub

---
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int subsetXORSum(int[] nums) {
+        int n = nums.length;
+        int ans = 0;
+        
+        for (int mask = 0; mask < (1 << n); mask++) {
+            int xor = 0;
+            for (int i = 0; i < n; i++) {
+                if ((mask & (1 << i)) != 0) {
+                    xor ^= nums[i];
+                }
+            }
+            ans += xor;
+        }
+        
+        return ans;
+    }
+}
