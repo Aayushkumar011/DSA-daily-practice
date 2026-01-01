f1dc6ceec39d741d1bf12a7dc935c1db77ea8c11
Aayush Kumar <roraayush0001@gmail.com>
2026-01-01 12:04:39 +0530

    Time: 0 ms (100%), Space: 43.3 MB (77.02%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,23 +1,45 @@
 class Solution {
     public int[] plusOne(int[] digits) {
+
+        // Length of the input array
         int n = digits.length;
-        int[] ans = new int[n+1];
-        int c=1;
-        for(int i=n-1;i>=0;i--){
-            digits[i]+=c;
-            if(digits[i]>9){
-                ans[i+1]=0;
+
+        // Result array of size n+1 to handle overflow cases like [9,9,9]
+        int[] ans = new int[n + 1];
+
+        // Carry initialized to 1 because we are adding one
+        int c = 1;
+
+        // Traverse digits from right to left (least significant to most)
+        for (int i = n - 1; i >= 0; i--) {
+
+            // Add carry to current digit
+            digits[i] += c;
+
+            // If digit becomes greater than 9, it turns into 0
+            // and carry remains 1
+            if (digits[i] > 9) {
+                ans[i + 1] = 0;
             }
+            // If digit is <= 9, store it and reset carry to 0
             else {
-                c=0;
-                ans[i+1]=digits[i];
+                c = 0;
+                ans[i + 1] = digits[i];
             }
         }
-    if(c==0){
-        int[] result = Arrays.copyOfRange(ans,1,n+1);
-        return result;
+
+        // If no carry is left, ignore the extra leading zero
+        // and return array from index 1 to n
+        if (c == 0) {
+            return Arrays.copyOfRange(ans, 1, n + 1);
         }
-    else ans[0]=1;
-    return ans;
+        // If carry is still present, set most significant digit to 1
+        // Example: [9] -> [1,0], [9,9] -> [1,0,0]
+        else {
+            ans[0] = 1;
+        }
+
+        // Return the final result
+        return ans;
     }
 }
