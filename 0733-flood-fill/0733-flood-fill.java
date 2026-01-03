67433b4fbbd91971fb2c28d15d756ca10116603c
Aayush Kumar <roraayush0001@gmail.com>
2026-01-03 11:37:52 +0530

    Time: 1 ms (44.54%), Space: 47.1 MB (11.63%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,19 +1,19 @@
 class Solution {
-    public static void dfs(int[][] image, int i, int j, int color,int n,int m,boolean[][] visited,int c){
-         if(i<0 || j<0 || i>=n || j>=m || visited[i][j] || image[i][j]!=c)return;
-        visited[i][j]=true;
+    public static void dfs(int[][] image, int i, int j, int color,int cur_color, int n, int m, boolean[][] v, int[][]dir){
+        if(i<0 || j<0 || i>=n || j>=m || v[i][j] || image[i][j]!=cur_color)return;
+        v[i][j]=true;
         image[i][j]=color;
-        int dir[][] = {{0,1},{1,0},{-1,0},{0,-1}};
         for(int k=0;k<4;k++){
-            dfs(image,i+dir[k][0],j+dir[k][1],color,n,m,visited,c);
+            dfs(image,i+dir[k][0],j+dir[k][1],color,cur_color,n,m,v,dir);
         }
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int n = image.length;
         int m = image[0].length;
-        int c = image[sr][sc];
+        int cur_color = image[sr][sc];
+        int[][] direction = {{0,1},{1,0},{0,-1},{-1,0}};
         boolean[][] visited = new boolean[n][m];
-        dfs(image,sr,sc,color,n,m,visited,c);
+        dfs(image,sr,sc,color,cur_color,n,m,visited,direction);
     return image;
     }
 }
