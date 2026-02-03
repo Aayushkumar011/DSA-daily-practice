05260969f03975995f998a49bf497b09dcf5be97
Aayush Kumar <roraayush0001@gmail.com>
2026-02-03 14:36:12 +0530

    Time: 0 ms (100%), Space: 44.9 MB (31.09%) - LeetHub

--- 3637-trionic-array-i/3637-trionic-array-i.java
+++ 3637-trionic-array-i/3637-trionic-array-i.java
@@ -1,22 +1,31 @@
 class Solution {
     public boolean isTrionic(int[] nums) {
-        int c=2;
-        int i=1;
-        if (nums[1] <= nums[0]) return false;
-        while(i<nums.length){
-            if (nums[i] == nums[i-1]) return false;
-            if(c==2 && nums[i]<nums[i-1]){
-                c=1;
+        int i = 1;
+        int n = nums.length;
+        int j=i;
+        while(i<n){
+            if(nums[i]>nums[i-1]){
+                i++;
             }
-            else if(c==1 && nums[i]>nums[i-1]){
-                c=0;
-            }
-            else if(c==0 && nums[i]<nums[i-1]){
-                return false;
-            }
-            i++;
+            else break;
         }
-    if(c==0)return true;
-    else return false;
+        if(j==i)return false;
+        else j=i;
+        while(i<n){
+            if(nums[i]<nums[i-1]){
+                i++;
+            }
+            else break;
+        }
+        if(j==i)return false;
+        else j=i;
+        while(i<n){
+            if(nums[i]>nums[i-1]){
+                i++;
+            }
+            else break;
+        }
+        if(j==i) return false; 
+    return i==n;
     }
 }
