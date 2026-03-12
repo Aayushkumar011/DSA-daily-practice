a5afecfe6a1792eea82313529986a399f801b23a
Aayush Kumar <roraayush0001@gmail.com>
2026-03-12 10:32:13 +0530

    Time: 2 ms (95.47%), Space: 45.8 MB (66.23%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,23 +1,20 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> com = new ArrayList<>();
-        int sum=0;
-        helper(ans,com,candidates,sum,target,0);
-        return ans;
-    }
-    public static void helper(List<List<Integer>> ans,List<Integer> com,int[] c,int sum,int target,int s){
-        if(sum==target){
-            ans.add(new ArrayList<>(com));
+    public static void help(int[] c, int t, List<List<Integer>> ans, int i,int sum,List<Integer> a){
+        if(i>=c.length){
+            if(sum==t){
+                ans.add(new ArrayList<>(a));
+            }
             return;
         }
-        if(sum>target)return;
-        for(int i=s;i<c.length;i++){
-            com.add(c[i]);
-            sum+=c[i];
-            helper(ans,com,c,sum,target,i);
-            sum-=c[i];
-            com.remove(com.size()-1);
-        }
+        if(sum>t)return;
+        help(c,t,ans,i+1,sum,a);
+        a.add(c[i]);
+        help(c,t,ans,i,sum+c[i],a);
+        a.remove(a.size()-1);
+    }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        help(candidates,target,ans,0,0,new ArrayList<>());
+    return ans;
     }
 }
