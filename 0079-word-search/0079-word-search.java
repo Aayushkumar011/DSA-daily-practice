0505fec36f2a92d404786df675682ade3e63f1ef
Aayush Kumar <roraayush0001@gmail.com>
2026-03-13 10:18:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,33 +1,33 @@
 class Solution {
-    public static boolean check(char[][] board, String word, int[][] a, int r, int c, int index, boolean[][] visited){
-        if(index==word.length())return true;
-        if(r<0 || c<0 || r>=board.length || c>=board[0].length || board[r][c]!=word.charAt(index)  || visited[r][c])return false;
-
-        visited[r][c] = true;
-        for(int i=0;i<4;i++){
-            if(check(board,word,a,r+a[i][0],c+a[i][1],index+1,visited)){
-                visited[r][c] = false;
+    public static boolean search(char[][] board,int n,int m, String word,int l,int i,int j,int k,boolean[][] vis,int[][] dir){
+        if(k==l)return true;
+        if(i<0 || j<0 || i>=n || j>=m || vis[i][j] ||  board[i][j]!=word.charAt(k))return false;
+        vis[i][j]=true;
+        for(int z=0;z<4;z++){
+            if(search(board,n,m,word,l,i+dir[z][0],j+dir[z][1],k+1,vis,dir)){
                 return true;
             }
         }
-        visited[r][c] = false;
-        return false;
+        vis[i][j]=false;
+    return false;
     }
     public boolean exist(char[][] board, String word) {
         int n = board.length;
         int m = board[0].length;
-        int k=0;
-        int[][] a = {{1,0},{-1,0},{0,1},{0,-1}};
-        boolean[][] visited = new boolean[n][m];
+        int l = word.length();
+        boolean ans = false;
+        boolean[][] vis = new boolean[n][m];
+        int[][] dir = {{1,0},{0,1},{-1,0},{0,-1}};
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(board[i][j]==word.charAt(k)){
-                    if(check(board,word,a,i,j,0,visited)){
-                        return true;
-                }
+                if(board[i][j]==word.charAt(0)){
+                    if(search(board,n,m,word,l,i,j,0,vis,dir)){
+                        ans=true;
+                        break;
+                    }
                 }
             }
         }
-        return false;
+    return ans;
     }
 }
